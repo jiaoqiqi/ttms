6970f7e0c447d3da6300ae0936c5fc94f562c143
jiaoqiqi <1176932165@qq.com>
2017-08-15 23:48:22 +0800

    add login method and map
    Signed-off-by: jiaoqiqi <1176932165@qq.com>

--- public/js/components/login.js
+++ public/js/components/login.js
@@ -1,6 +1,19 @@
 import React ,{Component} from 'react';
 
+
 export default class Login extends Component {
+
+    login(){
+        const userName = this.refs.userName.value;
+        const userPassword = this.refs.userPassword.value;
+
+        if(!userName || !userPassword){
+            alert("The name or password connot be empty");
+            return ;
+        }
+        this.props.onLogin({userName , userPassword});
+    }
+
 
     render() {
         return <div className="container-fluid back">
@@ -18,7 +31,7 @@ export default class Login extends Component {
                     <input type="password" id="password" ref="userPassword" className="form-control"/>
                 </div>
 
-                <button className='btn btn-primary  logButton' >登录</button>
+                <button className='btn btn-primary  logButton' onClick={this.login.bind(this)} >登录</button>
 
             </div>
         </div>;
--- public/js/containers/login.js
+++ public/js/containers/login.js
@@ -1,5 +1,20 @@
 import Login from '../components/login'
 import {connect} from "react-redux";
 
-export default connect()(Login);
+const mapStateToProps = (state) => {
+    return {
+        logSuccess: state.login.logSuccess
+    }
+}
+
+const mapDispatchToProps = (dispatch)=>{
+    return {
+        onLogin:(userInfo)=>{
+            dispatch({type:'LOGIN',data:userInfo})
+        },
+
+    }
+}
+
+export default connect(mapStateToProps,mapDispatchToProps)(Login);
 
